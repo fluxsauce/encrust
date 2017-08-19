9991b4e4ab640bcd699f04f4d511ce1780dec538
Jon Peck <jpeck@fluxsauce.com>
2017-08-19 11:37:51 -0700

    Helps if you read the manual.

--- bin/encrust.js
+++ bin/encrust.js
@@ -66,7 +66,7 @@ const event = {};
 merge(event, (get(options, '_all')));
 
 if (get(process, 'env.TRAVIS')) {
-  merge(event, envTravisCi());
+  merge(event, envTravisCi(get(process, 'env')));
 }
 
 console.log(event);
