26de0f50f4f21c364fa13a078b2ea842ebe361e3
Jon Peck <jpeck@fluxsauce.com>
2017-09-08 09:43:42 -0700

    Optional fields.

--- schema/event.js
+++ schema/event.js
@@ -22,8 +22,8 @@ module.exports = Joi.object().keys({
   commit: sha.required().label('The commit SHA that the current build is testing'),
   testResult: testResult.required().label('0 if build is successful, 1 if build is broken.'),
   pullRequest: positiveInteger.optional().label('The pull request number'),
-  pullRequestSha: sha.optional().label('The commit SHA of the HEAD commit of the PR'),
-  pullRequestBranch: Joi.string().token().required().label('The name of the branch from which the PR originated'),
+  pullRequestSha: sha.default('').optional().label('The commit SHA of the HEAD commit of the PR'),
+  pullRequestBranch: Joi.string().token().optional().label('The name of the branch from which the PR originated'),
   repoSlug: Joi.string().required().label('The slug of the repository'),
   language: Joi.string().token().required().label('Primary interpreting language name'),
 });
